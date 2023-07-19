5d00979c09307058681afb07460d0f2f4594874f
mecastor <mecastor87@gmail.com>
2023-07-19 17:36:23 +0100

    Moved static webpage to template directory

--- app.js
+++
@@ -1,7 +0,0 @@
-const openMenu = () => {
-  document.querySelector(".sidebar").classList.add("open");
-};
-
-const closeMenu = () => {
-  document.querySelector(".sidebar").classList.remove("open");
-};
